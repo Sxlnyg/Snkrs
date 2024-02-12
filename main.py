04fd6bbda633a57a4f405a1e1d6d18c889df75ef
Andris <buzasandrascsaba@gmail.com>
2024-02-12 22:25:57 +0100

    commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from pprint import pprint
+
+
+def read_sneakers_data(file_name):
+    sneakers_data = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            sneaker_info = line.strip().split(',')
+            sneaker_dict = {
+                'title': sneaker_info[0],
+                'color': sneaker_info[1],
+                'full_price': sneaker_info[2],
+                'current_price': sneaker_info[3],
+                'publish_date': sneaker_info[4]
+            }
+            sneakers_data.append(sneaker_dict)
+    return sneakers_data
+
+
+def sort_and_display_sneakers(sneakers, key_func):
+    sorted_sneakers = sorted(sneakers, key=key_func)
+    pprint(sorted_sneakers)
+
+
+def main():
+    file_name = 'sneakers.csv'
+    sneakers = read_sneakers_data(file_name)
+
+    print("Válasz, melyik szempont alapján rendezzem a cipőket? \n 1 - title, \n 2 - color, \n 3 - full price, \n 4 - current price,  \n 5 - publish date ")
+    choice = input("Add meg a lehetőség számát! ")
+
+    if choice == '1':
+        key_func = lambda x: x['title']
+    elif choice == '2':
+        key_func = lambda x: x['color']
+    elif choice == '3':
+        key_func = lambda x: x['full_price']
+    elif choice == '4':
+        key_func = lambda x: x['current_price']
+    elif choice == '5':
+        key_func = lambda x: x['publish_date']
+    else:
+        print("Invalid choice.")
+        return
+
+    sort_and_display_sneakers(sneakers, key_func)
+
+
+main()
